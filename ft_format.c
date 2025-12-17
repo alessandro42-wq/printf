55c4061b739f0e1c66fb2a3289eb2fe8ce46b9ad
alessandro42-wq <alessandro.messano@tim.it>
2025-12-17 16:47:09 +0100

    Update ft_format.c

--- ft_format.c
+++ ft_format.c
@@ -1,1 +1,20 @@
+#include "libftprintf.h"
 
+int	ft_format(va_list args, char c)
+{
+	if (c == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	if (c == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	if (c == 'd' || c == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	if (c == 'u')
+		return (ft_putunbr(va_arg(args, unsigned int)));
+	if (c == 'x' || c == 'X')
+		return (ft_puthex(va_arg(args, unsigned int), c));
+	if (c == 'p')
+		return (ft_putptr((unsigned long)va_arg(args, void *)));
+	if (c == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
