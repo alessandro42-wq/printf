c48e25f3b162e5f6baf46a7e0935482fdcd3cc5f
alessandro42-wq <alessandro.messano@tim.it>
2025-12-17 16:34:24 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,1 +1,22 @@
+#include "ft_printf.h"
 
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		i;
+	int		count;
+
+	va_start(args, format);
+	i = 0;
+	count = 0;
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+			count += ft_format(args, format[++i]);
+		else
+			count += ft_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
