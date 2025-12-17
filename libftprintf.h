66b43ba2abf6556cb6de6f3c30ae659c0e3d4eeb
alessandro42-wq <alessandro.messano@tim.it>
2025-12-17 17:17:21 +0100

    Create libftprintf.h

---
+++ libftprintf.h
@@ -0,0 +1,9 @@
+#ifndef LIBFTPRINTF_H
+#define LIBFTPRINTF_H
+
+#include "../libft/libft.h"   // Include libft una volta sola
+
+int ft_printf(const char *format, ...);
+/* altre dichiarazioni di ft_printf e funzioni interne */
+
+#endif
