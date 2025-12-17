5aa65300d955372417837ac1b88447c37b759cc2
alessandro42-wq <alessandro.messano@tim.it>
2025-12-17 16:51:38 +0100

    Create ft_puthex.c

---
+++ ft_puthex.c
@@ -0,0 +1,17 @@
+#include "libftprintf.h"
+
+int	ft_puthex(unsigned long n, char format)
+{
+	int		count;
+	char	*base;
+
+	count = 0;
+	if (format == 'x')
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	if (n >= 16)
+		count += ft_puthex(n / 16, format);
+	count += ft_putchar(base[n % 16]);
+	return (count);
+}
