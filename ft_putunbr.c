f408d1a152f8f3f113804c313e2a681e0df22359
alessandro42-wq <alessandro.messano@tim.it>
2025-12-17 16:53:38 +0100

    Create ft_putunbr.c

---
+++ ft_putunbr.c
@@ -0,0 +1,12 @@
+#include "libftprintf.h"
+
+int	ft_putunbr(unsigned long n)
+{
+	int	count;
+
+	count = 0;
+	if (n >= 10)
+		count += ft_putunbr(n / 10);
+	count += ft_putchar((n % 10) + '0');
+	return (count);
+}
