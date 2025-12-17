b5026edb05a9bb5d859675d77243887f9047eeea
alessandro42-wq <alessandro.messano@tim.it>
2025-12-17 17:19:46 +0100

    Update libftprintf.h

--- libftprintf.h
+++ libftprintf.h
@@ -4,6 +4,10 @@
 #include "../libft/libft.h"   // Include libft una volta sola
 
 int ft_printf(const char *format, ...);
+int	ft_putunbr(unsigned long n);
+int	ft_putptr(unsigned long ptr);
+int	ft_puthex(unsigned long n, char format);
+int	ft_format(va_list args, char c);
 /* altre dichiarazioni di ft_printf e funzioni interne */
 
 #endif
